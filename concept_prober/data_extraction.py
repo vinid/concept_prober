e88601cd0c243011948a6685490e8a4c89d9f3da
vinid <chiccobia@gmail.com>
2022-10-05 12:52:20 -0700

    fixing matcher

--- concept_prober/data_extraction.py
+++ concept_prober/data_extraction.py
@@ -1,7 +1,17 @@
 from tqdm import tqdm
 from multiprocessing import Pool
 import os
+from sklearn.metrics import classification_report
 from typing import List
+
+
+def combine_and_compute_performance(dataframe, instance_to_target):
+    grouped_targets = dataframe.groupby(["word", "predictions"]).count().reset_index()
+    sorted_targets = grouped_targets.sort_values(1, ascending=False).drop_duplicates(
+        0).sort_index()  # drop "other" predictions
+    test_y = [instance_to_target(k) for k in sorted_targets["word"]]
+
+    return classification_report(test_y, sorted_targets["predictions"])
 
 
 class FindWordTextOccurrence:
@@ -23,6 +33,7 @@ class FindWordTextOccurrence:
             matches = list(tqdm(pool.imap(self.func, sentences), total=len(sentences), position=0))
 
         with open(f"{output_location}", "w") as filino:
+            filino.write(f"word\tsentence\n")
             for k in matches:
                 for key, value in k:
                     filino.write(f"{key}\t{value.strip()}\n")
